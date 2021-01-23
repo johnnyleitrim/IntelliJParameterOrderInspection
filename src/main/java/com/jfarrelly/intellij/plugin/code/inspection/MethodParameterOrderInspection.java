4419a5dad787be2759dd39f2d1de060aa406743e
John Farrelly <jfarrelly@hubspot.com>
2021-01-23 18:51:16 +0000

    Add a configuration option to specify the minimum number of parameter order violations that must be present for problems to be registered with a call.

--- src/main/java/com/jfarrelly/intellij/plugin/code/inspection/MethodParameterOrderInspection.java
+++ src/main/java/com/jfarrelly/intellij/plugin/code/inspection/MethodParameterOrderInspection.java
@@ -1,16 +1,28 @@
 package com.jfarrelly.intellij.plugin.code.inspection;
 
+import javax.swing.JComponent;
+
 import org.jetbrains.annotations.NotNull;
 
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.codeInspection.ui.SingleIntegerFieldOptionsPanel;
 import com.intellij.psi.PsiElementVisitor;
 
 public class MethodParameterOrderInspection extends AbstractBaseJavaLocalInspectionTool {
 
+  public int minIncorrectParameters = 2;
+
   @NotNull
   @Override
   public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
-    return new MethodParameterOrderVisitor(holder);
+    return new MethodParameterOrderVisitor(holder, minIncorrectParameters);
+  }
+
+  @NotNull
+  @Override
+  public JComponent createOptionsPanel() {
+    return new SingleIntegerFieldOptionsPanel(
+        "Minimum incorrect parameters", this, "minIncorrectParameters");
   }
 }
